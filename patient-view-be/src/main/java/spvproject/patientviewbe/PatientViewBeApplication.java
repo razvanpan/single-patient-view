a74f15e0f5e47aa361789745552dca34c0827945
mihaigorasteanu <69308455+mihaigorasteanu@users.noreply.github.com>
2020-09-14 11:59:48 +0300

    React app and mvn project (#1)
    * maven project with SpringBoot added
    * react app for UI added

---
+++ patient-view-be/src/main/java/spvproject/patientviewbe/PatientViewBeApplication.java
@@ -0,0 +1,13 @@
+package spvproject.patientviewbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientViewBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientViewBeApplication.class, args);
+	}
+
+}
---
+++ patient-view-be/src/test/java/spvproject/patientviewbe/PatientViewBeApplicationTests.java
@@ -0,0 +1,13 @@
+package spvproject.patientviewbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientViewBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
