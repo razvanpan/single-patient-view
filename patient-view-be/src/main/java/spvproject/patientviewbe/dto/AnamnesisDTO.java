2142dd0c251f954b4a7f16e1501027d65f11e584
WHQ_NT_DOMAIN\CB083492 <cata_banu98@yahoo.com>
2020-10-02 12:42:19 +0300

    Implement DTO and change Model

--- patient-view-be/src/main/java/spvproject/patientviewbe/dto/AnamnesisDTO.java
+++ patient-view-be/src/main/java/spvproject/patientviewbe/dto/AnamnesisDTO.java
@@ -1,6 +1,6 @@
-package spvproject.patientviewbe.Model;
+package spvproject.patientviewbe.dto;
 
-public class AnamnesisModel {
+public class AnamnesisDTO {
     private int id;
     private String lastLabsWork;
     private String presentSymptoms;
@@ -9,7 +9,10 @@ public class AnamnesisModel {
     private String familyMedicalHistory;
     private String allergies;
 
-    public AnamnesisModel(int id, String lastLabsWork, String presentSymptoms, String medicines, String supplements, String familyMedicalHistory, String allergies) {
+    public AnamnesisDTO() {
+    }
+
+    public AnamnesisDTO(int id, String lastLabsWork, String presentSymptoms, String medicines, String supplements, String familyMedicalHistory, String allergies) {
         this.id = id;
         this.lastLabsWork = lastLabsWork;
         this.presentSymptoms = presentSymptoms;
@@ -23,27 +26,55 @@ public class AnamnesisModel {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getLastLabsWork() {
         return lastLabsWork;
+    }
+
+    public void setLastLabsWork(String lastLabsWork) {
+        this.lastLabsWork = lastLabsWork;
     }
 
     public String getPresentSymptoms() {
         return presentSymptoms;
     }
 
+    public void setPresentSymptoms(String presentSymptoms) {
+        this.presentSymptoms = presentSymptoms;
+    }
+
     public String getMedicines() {
         return medicines;
+    }
+
+    public void setMedicines(String medicines) {
+        this.medicines = medicines;
     }
 
     public String getSupplements() {
         return supplements;
     }
 
+    public void setSupplements(String supplements) {
+        this.supplements = supplements;
+    }
+
     public String getFamilyMedicalHistory() {
         return familyMedicalHistory;
     }
 
+    public void setFamilyMedicalHistory(String familyMedicalHistory) {
+        this.familyMedicalHistory = familyMedicalHistory;
+    }
+
     public String getAllergies() {
         return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
     }
 }
