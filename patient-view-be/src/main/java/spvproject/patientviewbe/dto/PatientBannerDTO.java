8fb19427d1bb920d1b4a04f991f7982430f14add
lorenaanghel <64457623+lorenaanghel@users.noreply.github.com>
2020-10-13 16:01:24 +0300

    created PatientBannerModel, PatientBannerController, endpoint and also tested in postman  (#6)
    * created PatientBannerModel and PatientBannerController
    * deleted /bin/ folder and renamed class PatientBannerModel into PatientBanner
    * renamed path for @RequestMapping
    * added classes Service, Repository and DTO for integrating be with fe
    * deleted conflicted file
    * deleted bin folder

---
+++ patient-view-be/src/main/java/spvproject/patientviewbe/dto/PatientBannerDTO.java
@@ -0,0 +1,100 @@
+package spvproject.patientviewbe.dto;
+
+public class PatientBannerDTO {
+	private String ID;
+	private String name;
+	private String gender;
+	private int age;
+	private String birthDate;
+	private String lengthStay;
+	private String ED;
+	private String allergies;
+	private String eID;
+	private String triage;
+	private String risks;
+	
+	public PatientBannerDTO() {}
+	
+	public PatientBannerDTO(String iD, String name, String gender, int age, String birthDate, String lengthStay,
+			String eD, String allergies, String eID, String triage, String risks) {
+		this.ID = iD;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.birthDate = birthDate;
+		this.lengthStay = lengthStay;
+		this.ED = eD;
+		this.allergies = allergies;
+		this.eID = eID;
+		this.triage = triage;
+		this.risks = risks;
+	}
+
+
+
+	public String getID() {
+		return ID;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+
+
+	public String getLengthStay() {
+		return lengthStay;
+	}
+
+
+
+	public String getED() {
+		return ED;
+	}
+
+
+
+	public String getAllergies() {
+		return allergies;
+	}
+
+
+
+	public String geteID() {
+		return eID;
+	}
+
+
+
+	public String getTriage() {
+		return triage;
+	}
+
+
+
+	public String getRisks() {
+		return risks;
+	}
+	
+	
+}
