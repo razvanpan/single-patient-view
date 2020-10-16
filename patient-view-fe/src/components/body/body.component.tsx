d0189f58bfd2d96340f5c5ee6c674578ed22f49b
MezeiSebastian <61548741+MezeiSebastian@users.noreply.github.com>
2020-10-16 13:36:27 +0300

    added physical examination component

--- patient-view-fe/src/components/body/body.component.tsx
+++ patient-view-fe/src/components/body/body.component.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './body.component.css'
 
+import PhysicalExamination from '../physical-examination/physical-examination.component'
+
+
 const BodyContainer = () => (
   <div> <div className={'card-area'}>
-    Card-area
+    <PhysicalExamination/>
     </div>
     <div className={'work-panel'}>
     Work-panel
