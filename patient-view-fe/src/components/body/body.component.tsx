be25a72ab5cb8dbaf428aef56ead7af27df54365
Alex <lung_ali@yahoo.com>
2020-10-19 10:02:05 +0300

    Merge branch 'master' into chief-complaint-frontend

--- patient-view-fe/src/components/body/body.component.tsx
+++ patient-view-fe/src/components/body/body.component.tsx
@@ -3,13 +3,18 @@ import "./body.component.css";
 import ChiefComplaint from "chief-complaint-lung-alexandru";
 import "chief-complaint-lung-alexandru/dist/index.css";
 
+import AnamnesisComponent from "../anamnesis/anamnesis.component";
+
 const BodyContainer = () => (
   <div>
-    {" "}
+    <div className={"body-container"}></div>
     <div className={"card-area"}>
       Card-area
       <div className={"chief-complaint"}>
         <ChiefComplaint></ChiefComplaint>
+      </div>
+      <div>
+        <AnamnesisComponent></AnamnesisComponent>
       </div>
     </div>
     <div className={"work-panel"}>Work-panel</div>
