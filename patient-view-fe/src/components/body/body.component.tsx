ea3e9a1773f73432c435594fa1d671be4b9ea7f0
mihaialexandru <mihai.anghel970316@gmail.com>
2020-10-19 14:05:08 +0300

    Integrate vital signs component in spv

--- patient-view-fe/src/components/body/body.component.tsx
+++ patient-view-fe/src/components/body/body.component.tsx
@@ -3,12 +3,14 @@ import "./body.component.css";
 import PhysicalExamination from "../physical-examination/physical-examination.component";
 import AnamnesisComponent from "../anamnesis/anamnesis.component";
 import ChiefComplaintComponent from "../chief-complaint/chief-complaint.component";
+import VitalSignComponent from "../vital-signs/vital-signs.component";
 
 const BodyContainer = () => (
   <div className={"body-container"}>
     <div className={"card-area"}>
       <ChiefComplaintComponent></ChiefComplaintComponent>
       <PhysicalExamination />
+      <VitalSignComponent />
       <AnamnesisComponent></AnamnesisComponent>
     </div>
     <div className={"work-panel"}>Work-panel</div>
