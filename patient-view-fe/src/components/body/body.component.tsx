0c6a52e4a7c8651bb89f50b0f104a3bcd5780c7e
WHQ_NT_DOMAIN\EI083499 <emilbdb@gmail.com>
2020-10-19 13:42:47 +0300

    added PainAssessment to framework

--- patient-view-fe/src/components/body/body.component.tsx
+++ patient-view-fe/src/components/body/body.component.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import './body.component.css'
-import PhysicalExamination from '../physical-examination/physical-examination.component'
-import AnamnesisComponent from '../anamnesis/anamnesis.component';
-
+import React from "react";
+import "./body.component.css";
+import PhysicalExamination from "../physical-examination/physical-examination.component";
+import AnamnesisComponent from "../anamnesis/anamnesis.component";
+import PainAssessmentComponent from "../pain-assessment/pain-assessment";
 const BodyContainer = () => (
-  <div className={'body-container'}>
-     <div className={'card-area'}>
-       <PhysicalExamination/>
-       <AnamnesisComponent></AnamnesisComponent>
+  <div className={"body-container"}>
+    <div className={"card-area"}>
+      <PainAssessmentComponent></PainAssessmentComponent>
+      <PhysicalExamination />
+      <AnamnesisComponent></AnamnesisComponent>
     </div>
-    <div className={'work-panel'}>
-    Work-panel
+    <div className={"work-panel"}>Work-panel</div>
   </div>
-  </div>
- 
 );
 
 export default BodyContainer;
