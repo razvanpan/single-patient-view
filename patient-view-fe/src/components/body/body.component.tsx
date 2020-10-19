c9367e73b53f2be11046a10c66e7dc57264276ac
Alex Lung <lung_ali@yahoo.com>
2020-10-19 11:11:48 +0300

    Revert "Added the chief complaint card to main project"

--- patient-view-fe/src/components/body/body.component.tsx
+++ patient-view-fe/src/components/body/body.component.tsx
@@ -1,20 +1,19 @@
-import React from "react";
-import "./body.component.css";
-import PhysicalExamination from "../physical-examination/physical-examination.component";
-import AnamnesisComponent from "../anamnesis/anamnesis.component";
-import ChiefComplaint from "chief-complaint-lung-alexandru";
+import React from 'react';
+import './body.component.css'
+import PhysicalExamination from '../physical-examination/physical-examination.component'
+import AnamnesisComponent from '../anamnesis/anamnesis.component';
 
 const BodyContainer = () => (
-  <div className={"body-container"}>
-    <div className={"card-area"}>
-      <PhysicalExamination />
-      <AnamnesisComponent></AnamnesisComponent>
-      <div className={"chief-complaint"}>
-        <ChiefComplaint></ChiefComplaint>
-      </div>
+  <div className={'body-container'}>
+     <div className={'card-area'}>
+       <PhysicalExamination/>
+       <AnamnesisComponent></AnamnesisComponent>
     </div>
-    <div className={"work-panel"}>Work-panel</div>
+    <div className={'work-panel'}>
+    Work-panel
   </div>
+  </div>
+ 
 );
 
 export default BodyContainer;
